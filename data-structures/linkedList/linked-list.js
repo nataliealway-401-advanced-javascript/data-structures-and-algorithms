125323e34881c97deeb54b030e55b55b66fb29f0
natalielyn <nlmaxwell@gmail.com>
2020-01-10 17:03:47 -0800

    created node and linkedlist classes

---
+++ data-structures/linkedList/linked-list.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+//Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
+
+
+//Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
+
+//Define a method called toString (or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List, formatted as:
