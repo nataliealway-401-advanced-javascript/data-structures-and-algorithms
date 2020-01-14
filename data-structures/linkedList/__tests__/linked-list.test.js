7de4a0dba1fe4b60c11fc6371afa8676ce1ee90f
natalielyn <nlmaxwell@gmail.com>
2020-01-14 14:50:18 -0800

    all tests passing

--- data-structures/linkedList/__tests__/linked-list.test.js
+++ data-structures/linkedList/__tests__/linked-list.test.js
@@ -161,8 +161,27 @@ describe('Testing Kth from the end of a linked list,', () => {
   list.insert('5');
 
   it('Should return exception when k is greater then the length of the linked list', () => {
-    expect(list.kthFromTheEnd('10')).toBe('Exception');
+    expect(list.kthFromTheEnd('6')).toBe('Exception');
     expect(list.kthFromTheEnd('87')).toBe('Exception');
+  });
+
+  it('Should return Exception Wwhen k and the length of the list are the same', () => {
+    expect(list.kthFromTheEnd('5')).toBe('Exception');
+  });
+
+  it('Should return Exception when k is not a positive integer', () => {
+    expect(list.kthFromTheEnd('-4')).toBe('Exception');
+  });
+
+  const singleList = new LinkedList();
+  singleList.insert = ('1');
+
+  it('Should return an exception when the linked list contains one and k is 0', () => {
+    expect(singleList.kthFromTheEnd(0)).toBe('Exception');
+  });
+
+  it('Should return an exception when the list contain one node and k is greater then 1', () => {
+    expect(singleList.kthFromTheEnd('5')).toBe('Exception');
   });
 
 });
