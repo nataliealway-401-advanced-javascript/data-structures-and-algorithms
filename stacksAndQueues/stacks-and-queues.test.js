1d3dc0e157a649734af671113a8c40c35214b72d
natalielyn <nlmaxwell@gmail.com>
2020-01-18 16:17:19 -0800

    created queue methods

--- stacksAndQueues/stacks-and-queues.test.js
+++ stacksAndQueues/stacks-and-queues.test.js
@@ -7,6 +7,8 @@ class Node {
   }
 }
 
+//_____________ STACKS ________________
+
 class Stack {
   constructor(){
     this.top = null;
@@ -36,7 +38,46 @@ class Stack {
     } else {
       return false;
     }
+}
+
+  // ________ QEUES  __________
+
+  class Queue {
+    constructor() {
+      this.rear = null;
+      this.front = null;
+    }
+  
+   
+    enqueue(value) {
+      let node = new Node(value);
+      //If there is no front node, this node will be the front and the rear
+      if(!this.front) return this.front = node, this.rear = node;
+      //Otherwise this node will become the rear and have its next be the previous rear
+      this.rear.next = node;
+      return this.rear = node;
+    }
+  
+    dequeue() {
+      //Declare a temp variable to reference the front
+      let temp = this.front;
+      //Change the front to the the front's next node
+      this.front = this.front.next;
+      //If the front is null set the rear to be null as well
+      if(!this.front)this.rear = null;
+      temp.next = null;
+      return temp;
+    }
+    //Look at the front of the queue
+    peek() {
+      return this.front;
+    }
+  
   }
+  
+  module.exports = {Queue, Stack};
+
+
 
 
 
