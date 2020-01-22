fd5bcaba1082409b4fb55806ae4de4c46ee77d70
natalielyn <nlmaxwell@gmail.com>
2020-01-21 23:38:41 -0800

    all tests passing

--- challenges/fifoAnimalShelter/__tests__/fifo-animal-shelter.test.js
+++ challenges/fifoAnimalShelter/__tests__/fifo-animal-shelter.test.js
@@ -9,4 +9,32 @@ describe('AnimalShelter testing..', () => {
     expect(shelter.in.isEmpty()).toBeTruthy();
     expect(shelter.out.isEmpty()).toBeTruthy();
   });
+  describe('enqueue into the animal shelter', () => {
+    let shelter = new animalShelter;
+   
+  
+    it('should push to the first stack if empty', () => {
+      shelter.enqueue('dog');
+      expect(shelter.in.top.value).toEqual('dog');
+    });
+    it('should push to stack with multiple dog and cat values', () => {
+      shelter.enqueue('dog');
+      shelter.enqueue('cat');
+      shelter.enqueue('cat');
+      expect(shelter.in.top.value).toBe('cat');
+    });
+  });
+
+  describe('dequeue from the animal shelter', () => {
+    let shelter = new animalShelter;
+  
+    it('returns null if pref is not "dog" or "cat"', () => {
+      shelter.enqueue('cat');
+      shelter.enqueue('dog');
+      shelter.enqueue('cat');
+  
+      expect(shelter.dequeue('bird')).toBeNull();
+
+    });
+  });
 });
