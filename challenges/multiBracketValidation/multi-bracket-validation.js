6eab6b0463e4ec4359727714e7eb88422aae482f
natalielyn <nlmaxwell@gmail.com>
2020-01-22 15:41:30 -0800

    added working code made in repl

---
+++ challenges/multiBracketValidation/multi-bracket-validation.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let matchingBrackets = function (str) {
+  let stack = [];
+  let brackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+      stack.push(str[i]);
+    }
+    else {
+      let last = stack.pop();
+      if (str[i] !== brackets[last]) {
+        return false;
+      }
+    }
+  }
+  if (stack.length !== 0) {
+    return false;
+  }
+
+  return true;
+};
+
+
+module.exports = matchingBrackets();
